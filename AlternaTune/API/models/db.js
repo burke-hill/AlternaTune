4c6db0fbe08d7c0bb127774c3f323ec0c0ea5518
burke-hill <charlesmccampbellhill@gmail.com>
2022-09-22 19:00:43 -0400

    we went live :)

--- AlternaTune/API/models/db.js
+++ AlternaTune/API/models/db.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 const express = require('express');
 
-mongoose.connect('mongodb://localhost:27017/AlternatuneDB');
-const app = express();
+//mongoose.connect('mongodb://localhost:27017/AlternatuneDB');
 
+
+const app = express();
+mongoose.connect('mongodb+srv://dbUser:alterapp@cluster0.zhvhzv8.mongodb.net/AlternatuneDB?retryWrites=true&w=majority');
 app.get("/", (req, res) => {
     res.send("Updated!");
 });
