c8c69bab39a363829156ade59ccb67e3b86c2f68
burke-hill <charlesmccampbellhill@gmail.com>
2022-09-23 02:10:35 -0400

    added a procfile

--- AlternaTune/API/models/db.js
+++ AlternaTune/API/models/db.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose');
 const express = require('express');
-const dotenv = require("dotenv");
+const path = require('path')
+const dotenv = require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
+
 
 
 //mongoose.connect('mongodb://localhost:27017/AlternatuneDB');
 
-dotenv.config();
+
 const app = express();
-mongoose.connect('mongodb+srv://{process.env.DB_USERNAME}:{process.env.DB_PASSWORD}@cluster0.zhvhzv8.mongodb.net/AlternatuneDB?retryWrites=true&w=majority');
+mongoose.connect(process.env.MONGO_URL);
+
+
 app.get("/", (req, res) => {
     res.send("Updated!");
 });
