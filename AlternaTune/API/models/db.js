fc2b5d3f65f679df868418eca9beef176ede3f02
burke-hill <charlesmccampbellhill@gmail.com>
2022-09-22 22:43:52 -0400

    fixed a scary leak :)

--- AlternaTune/API/models/db.js
+++ AlternaTune/API/models/db.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 const express = require('express');
+const dotenv = require("dotenv");
+
 
 //mongoose.connect('mongodb://localhost:27017/AlternatuneDB');
 
-
+dotenv.config();
 const app = express();
-mongoose.connect('mongodb+srv://dbUser:alterapp@cluster0.zhvhzv8.mongodb.net/AlternatuneDB?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://{process.env.DB_USERNAME}:{process.env.DB_PASSWORD}@cluster0.zhvhzv8.mongodb.net/AlternatuneDB?retryWrites=true&w=majority');
 app.get("/", (req, res) => {
     res.send("Updated!");
 });
